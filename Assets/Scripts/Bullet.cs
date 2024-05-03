c611b794507e1ad56613ee9f7195818675afd124
Samin <saminmohammed65@gmail.com>
2024-05-03 15:24:16 +0600

    Added basic shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Bullet : MonoBehaviour
+{
+    [Range(1, 10)]
+    [SerializeField] private float speed;
+
+    [Range(1, 10)]
+    [SerializeField] private float lifeTime;
+
+    private Rigidbody2D bulletRb;
+
+    private void Start()
+    {
+        bulletRb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void FixedUpdate()
+    {
+        bulletRb.velocity = transform.up * speed;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Variables
+
+    // Gun variables
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+
+    [Range(0.1f, 2f)]
+    [SerializeField] private float fireRate;
+
+    private float fireTimer;
+
+    // Player variables
     [SerializeField] private float speed;
-    [SerializeField] private KeyCode pullBeamInput;
-    [SerializeField] private KeyCode deathBeamInput;
 
     private Rigidbody2D playerRb;
 
@@ -14,6 +24,8 @@ public class PlayerController : MonoBehaviour
     private float my;
 
     private Vector2 mousePos;
+
+    #endregion
 
     private void Start()
     {
@@ -23,18 +35,40 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         // Initialize player and mouse movement
-        mx = Input.GetAxisRaw("Horizontal");
-        my = Input.GetAxisRaw("Vertical");
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Movement();
 
-        float angle = Mathf.Atan2(mousePos.y - transform.position.y, 
-            mousePos.x - transform.position.y) * Mathf.Rad2Deg - 90f;
-
-        transform.localRotation = Quaternion.Euler(0, 0, angle);
+        // Handle Shooting
+        if(Input.GetMouseButton(0) & fireTimer <= 0f)
+        {
+            Shoot();
+            fireTimer = fireRate;
+        }
+        else
+        {
+            fireTimer -= Time.deltaTime;
+        }
     }
     private void FixedUpdate()
     {
         // Handle player movement speed
         playerRb.velocity = new Vector2 (mx, my).normalized * speed;
     }
+
+    #region Functions
+    private void Movement()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        my = Input.GetAxisRaw("Vertical");
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle = Mathf.Atan2(mousePos.y - transform.position.y,
+            mousePos.x - transform.position.y) * Mathf.Rad2Deg - 90f;
+
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+    private void Shoot()
+    {
+        Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
+    }
+    #endregion
 }
